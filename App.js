cfffca40fbf5f39b690aa913ab88f357366ba6b3
ggsonson <113072348+ggsonson@users.noreply.github.com>
2022-09-15 20:30:30 -0400

    Update App.js
    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Switch } from 'react-native';
+import React from 'react';
 
-export default function App() {
+function TwoText(props){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textView}>{props.name}</Text>
+      <Text style={styles.textView}>{props.value}</Text>
     </View>
-  );
+  )
+}
+
+export default function App() {
+  const [value, setValue] = React.useState('Type something...')
+  const [isEnabled, setIsEnabled] = React.useState(false);
+  const toggleSwitch = () => setIsEnabled(enabled => !enabled);
+  return (
+    <View style={[styles.container,(isEnabled ? styles.black : styles.white)]}>
+
+      <TextInput 
+        style = {styles.textInput}
+        onChangeText = {text => setValue(text)} />
+      <Text style={styles.textView}>{value}</Text>
+
+      <Button title='Clear' onPress={() => setValue('')}></Button>
+
+      <Switch 
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      ></Switch>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: '#aaa',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%'
   },
+  textView: {
+    fontSize: 24,
+    color: '#00f'
+  },
+  textInput: {
+    color: '#aaa',
+    borderBottomColor: '#aaa',
+    borderWidth: '1px',
+    padding: 5,
+    width: '90%'
+  },
+  black: {
+    backgroundColor: '#000'
+  },
+  white: {
+    backgroundColor: '#fff'
+  }
 });
